bd894c4d31b97ec73228d1655b38d083ebc1a31b
T.C. Chiu <freexyz2007@gmail.com>
2015-03-24 17:37:39 +0800

    add dbglock but dont work

---
+++ ti/BLE-CC254x-1.4.0/projects/ble/wcube/source/dbglock.c
@@ -0,0 +1,37 @@
+/*
+ ******************************************************************************
+ * dbglock.c - flash lock bit
+ *
+ * Copyright (c) 2014-2016 by ZealTek Electronic Co., Ltd.
+ *
+ * This software is copyrighted by and is the property of ZealTek
+ * Electronic Co., Ltd. All rights are reserved by ZealTek Electronic
+ * Co., Ltd. This software may only be used in accordance with the
+ * corresponding license agreement. Any unauthorized use, duplication,
+ * distribution, or disclosure of this software is expressly forbidden.
+ *
+ * This Copyright notice MUST not be removed or modified without prior
+ * written consent of ZealTek Electronic Co., Ltd.
+ *
+ * ZealTek Electronic Co., Ltd. reserves the right to modify this
+ * software without notice.
+ *
+ * History:
+ *	2015.03.17	T.C. Chiu	frist edition
+ *
+ ******************************************************************************
+ */
+
+/*
+ ******************************************************************************
+ *
+ * dbglock bits
+ *
+ ******************************************************************************
+ */
+#pragma location="FLASH_LOCK_BITS"
+__root const unsigned char	__dbglock[16] = {
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x3F
+};
+
